5e129acf2108d830e717602c21855a8a9e47bce9
JayEee2U <j_double_u@icloud.com>
2023-12-23 16:25:10 -0500

    Add mixins and use, partials folder

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,33 +1,96 @@
-import videos from '../../data/video-details.json';
+import videosDetails from '../../data/video-details.json';
 import './Body.scss';
+import likes from '../../assets/images/likes.svg'
+import upload from '../../assets/images/upload.svg'
+import MohanM from '../../assets/images/Mohan-muruge.jpg'
+import videos from '../../data/videos.json'
 
-const video = videos[0]
+const video = videosDetails[0]
 
 function Body() {
-    console.log(videos)
+    console.log(videosDetails)
     return (
         <>
         <div className='container-image'>
             <img  className='image_Red-Cow' src={video.image} alt=' ' />
         </div>
-        <div className='h1_div'>
-            <h1>Text</h1>
+        <div className='comment'>
+            <h1>{video.title}</h1>
+            <div className='divider'>&nbsp;</div>
         </div>
         <div>
-            <div>
-                <h2>Text</h2>
+            <div className='commentator'>
+                <h3>By {video.channel}</h3>
             </div>
             <div>
                 <h3>Text</h3>
             </div>
             <div>
-                <img src='' alt='' />
+                <img src={upload} alt='' />
             </div>
             <div>
-                <img src='' alt='' />
+                <img src={likes} alt='' />
+                <div className='divider'>&nbsp;</div>
             </div>
          </div>
+        <div className='video-commentary'>
+            <p className='video-commentary__paragraph'>{video.description}
+            </p>
+        </div>
+        <div>
+        <div className='avatar-div'>
+                    <img  className="avatar" src={MohanM} alt="Mohan Muruge" />
+                </div>
+        </div>
+        <div>
+            <h3 className='nextHeader'>JOIN THE CONVERSATION</h3>
+        </div>
+        <div>
+            <input type='text' placeholder='Add a new comment'></input>
+        </div>
+        <div>
+            <button type='button'>COMMENT</button>
+            <div className='divider'>&nbsp;</div>
+        </div>
 
+        {video.comments.map( c => 
+                <div className='commentSection'>
+                    <div>
+                        <h3>{c.name}</h3>
+                    </div>
+                    <div>
+                        <p>{c.comment}</p>
+                    </div>
+                    <div>
+                        <h3>{c.timestamp}</h3>
+                    </div>
+                    <div className='divider'>&nbsp;</div>
+                </div>
+            )
+        }
+            <div>
+                <h3 className='nextHeader'>NEXT VIDEOS</h3>
+            </div>
+
+        <div>
+        {videos.map( v => 
+            <div className='videoSection'>
+                <div className='nextVideoDiv'>
+                 <img className='nextVideoImage' src={v.image} alt=''/>
+                </div>
+            <div>
+                <h3>{v.title}</h3>
+            </div>
+            <div>
+                <p>{v.channel}</p>
+            </div>
+        </div>
+            )
+        }
+
+
+        </div>
+            
         </>
         );
     }
