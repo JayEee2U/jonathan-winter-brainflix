c87ed3b9e881b1d2761b1f10b09162967a7506b3
JayEee2U <j_double_u@icloud.com>
2023-12-22 23:22:43 -0500

    Size video image, ad icons to assets folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header/Header';
+import Body from './components/Body/Body';
 import axios from 'axios';
 
 
 function App() {
   return (
+    <>
     <Header />
-
+    <Body />
+  </>
+    
   );
 }
 
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,34 @@
-import Videos from '../../data/videos.json';
+import videos from '../../data/video-details.json';
+import './Body.scss';
+
+const video = videos[0]
 
 function Body() {
+    console.log(videos)
     return (
+        <>
+        <div className='container-image'>
+            <img  className='image_Red-Cow' src={video.image} alt=' ' />
+        </div>
+        <div className='h1_div'>
+            <h1>Text</h1>
+        </div>
+        <div>
+            <div>
+                <h2>Text</h2>
+            </div>
+            <div>
+                <h3>Text</h3>
+            </div>
+            <div>
+                <img src='' alt='' />
+            </div>
+            <div>
+                <img src='' alt='' />
+            </div>
+         </div>
 
-
-
-
+        </>
         );
     }
 
