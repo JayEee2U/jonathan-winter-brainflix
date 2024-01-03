3a84ac4e721be85cb3df00d5526b8a0e68b8d09e
JayEee2U <j_double_u@icloud.com>
2024-01-02 20:13:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
+import Page from './pages/Page/Page';
 
 
 
 function App() {
   return (
-    <>
+  <>
     <Header />
-    <Body />
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Body />}/>
+        <Route path='/pages' element={<Pages />}/>
+      </Routes>
+    </BrowserRouter>
   </>
-    
   );
 }
 
